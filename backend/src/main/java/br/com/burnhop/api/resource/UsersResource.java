38ea41adf67069032826baac7f1f823e628e1b7c
Matthew2812 <matheus_soares507@hotmail.com>
2020-09-19 16:54:32 -0300

    Refatorando código

--- backend/src/main/java/br/com/burnhop/api/resource/UsersResource.java
+++ backend/src/main/java/br/com/burnhop/api/resource/UsersResource.java
@@ -1,24 +1,15 @@
 package br.com.burnhop.api.resource;
 
 import br.com.burnhop.model.Users;
-import br.com.burnhop.model.Login;
 
-import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.sql.Timestamp;
-import java.sql.Date;
-import java.util.Base64;
 
 import br.com.burnhop.repository.UsersRepository;
 import br.com.burnhop.repository.LoginRepository;
 import br.com.burnhop.api.controller.UserController;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-
-import javax.websocket.server.PathParam;
 
 @RestController()
 @CrossOrigin("*")
@@ -31,7 +22,7 @@ public class UsersResource {
         userController = new UserController(login_repository, user_repository);
     }
 
-    @PostMapping(value = "/create")
+    @PostMapping()
     public ResponseEntity<String> createUser(@RequestParam("name") String name, @RequestParam("username") String username,
      @RequestParam("data_nasc") String date, @RequestParam("email") String email, @RequestParam("pwd") String pwd) throws NoSuchAlgorithmException {
 
@@ -42,8 +33,8 @@ public class UsersResource {
         return new ResponseEntity<>(user+"\n", HttpStatus.OK);
     }
 
-    @PostMapping("/authenticate")
-    public ResponseEntity<String> authenticate(
+    @PostMapping("/login")
+    public ResponseEntity<String> login(
             @RequestHeader(value = "email") String email,
             @RequestHeader(value = "password") String password) throws NoSuchAlgorithmException {
 
@@ -59,8 +50,9 @@ public class UsersResource {
             return new ResponseEntity<>("Email nao cadastrado\n", HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
+
     @GetMapping("/{email}")
-    public ResponseEntity<String> getEmailLogin(
+    public ResponseEntity<String> getUser(
             @PathVariable(value = "email") String email) {
 
         Users user = userController.getUserByEmail(email);
--- backend/src/main/java/br/com/burnhop/repository/LoginRepository.java
+++ backend/src/main/java/br/com/burnhop/repository/LoginRepository.java
@@ -1,13 +1,11 @@
 package br.com.burnhop.repository;
 
 import br.com.burnhop.model.Login;
-import br.com.burnhop.model.Users;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-import java.util.List;
 
 @RepositoryRestResource(collectionResourceRel = "login", path = "login")
 public interface LoginRepository extends CrudRepository<Login, Long> {
