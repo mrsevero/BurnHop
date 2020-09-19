22f25187f16109194e773d2f1ee2acc092d844fb
Bruno Severo <brunoseverocamilo@gmail.com>
2020-09-19 20:39:32 -0300

    Finalização da tela de cadastro e login integradas com api

--- front-end/src/cadastro/cadastro.js
+++ front-end/src/cadastro/cadastro.js
@@ -18,16 +18,23 @@ function registerUser(){
 
 function Cadastro(nome, email, data, senha){
   let http = new XMLHttpRequest()
-  let url = 'http://localhost:9000/users/create'
+  let url = 'http://localhost:9000/users/'
   let dados = `name= ${nome}&username=${nome}&email=${email}&data_nasc=${data}&pwd=${senha}`
   http.open('POST', url, true)
   http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded')
   http.setRequestHeader('Access-Control-Allow-Origin', '*')
-  http.onreadystatechange = function(){
+  http.onload = function(){
     console.log(http.responseText)
+    if(http.readyState == 4 && http.status == 200) {
+      alert("Cadastro realizado com sucesso!");
+      window.location.href = "../index.html";
+    }
+    else{
+      alert("Erro ao cadastrar, por favor verifique seus dados!")
+    }
   }
   http.send(dados);
-  }
+}
 
 
 function validaSenha() {
