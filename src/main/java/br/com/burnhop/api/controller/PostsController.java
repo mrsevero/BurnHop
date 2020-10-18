0c20d43996cc677c5874f79dd8e944ab7a85c599
Matthew2812 <matheus_soares507@hotmail.com>
2020-10-18 17:43:58 -0300

    #44 Adicionado métodos para criar e retornar posts

--- src/main/java/br/com/burnhop/api/controller/PostsController.java
+++ src/main/java/br/com/burnhop/api/controller/PostsController.java
@@ -2,8 +2,10 @@ package br.com.burnhop.api.controller;
 
 import br.com.burnhop.model.Dto.UserDto;
 import br.com.burnhop.model.Login;
+import br.com.burnhop.model.Posts;
 import br.com.burnhop.model.Users;
 import br.com.burnhop.repository.LoginRepository;
+import br.com.burnhop.repository.PostsRepository;
 import br.com.burnhop.repository.UsersRepository;
 
 import java.security.MessageDigest;
@@ -12,54 +14,21 @@ import java.util.Base64;
 
 public class PostsController {
 
-    LoginRepository login_repository;
-    UsersRepository user_repository;
+    PostsRepository posts_repository;
 
-    public PostsController(LoginRepository loginRepository, UsersRepository usersRepository) {
-        this.login_repository = loginRepository;
-        this.user_repository = usersRepository;
+    public PostsController(PostsRepository postsRepository) {
+        this.posts_repository = postsRepository;
     }
 
-    public UserDto createUser(Users newUser) throws NoSuchAlgorithmException {
-        Login login = getLoginByEmail(newUser.getLogin().getEmail());
-        if(login == null){
-            newUser.getLogin().setPassword(hashPassword(newUser.getLogin().getPassword()));
-            login_repository.save(newUser.getLogin());
-            user_repository.save(newUser);
+    public void createPost(Posts post) {
 
-            UserDto user = getUserByEmail(newUser.getLogin().getEmail());
-
-            return user;
-        }
-        return null;
     }
 
-    public boolean authenticateUser(String email, String password) throws NoSuchAlgorithmException {
+    public void getAllPosts() {
 
-        Login login = getLoginByEmail(email);
-        if(login == null){
-            throw new IllegalAccessError("E-mail não cadastrado");
-        }
-        String hashPassword = hashPassword(password);
-
-        return MessageDigest.isEqual(hashPassword.getBytes(), login.getPassword().getBytes());
     }
 
-    public UserDto getUserByEmail(String email) {
-        return new UserDto(user_repository.findByEmail(email));
-    }
-
-    public Login getLoginByEmail(String email) {
-        return login_repository.findByEmail(email);
-    }
-
-    private String hashPassword(String password) throws NoSuchAlgorithmException {
-
-        MessageDigest crypt = MessageDigest.getInstance("SHA-1");
-        crypt.reset();
-        crypt.update(password.getBytes());
-        byte[] hash = crypt.digest();
-
-        return Base64.getEncoder().encodeToString(hash);
+    public void getPostByEmail() {
+        
     }
 }
