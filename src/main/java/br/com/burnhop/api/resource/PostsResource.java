c57841aae78df7131aee6019a6e09551dfbe8752
Matthew2812 <matheus_soares507@hotmail.com>
2020-10-18 23:34:07 -0300

    #10 Corrigido requisições de post

--- src/main/java/br/com/burnhop/api/resource/PostsResource.java
+++ src/main/java/br/com/burnhop/api/resource/PostsResource.java
@@ -4,7 +4,7 @@ import br.com.burnhop.api.controller.PostsController;
 import br.com.burnhop.model.Dto.PostDto;
 import br.com.burnhop.model.Dto.CreatedPostDto;
 import br.com.burnhop.model.Posts;
-import br.com.burnhop.repository.LoginRepository;
+import br.com.burnhop.repository.ContentRepository;
 import br.com.burnhop.repository.UsersRepository;
 import br.com.burnhop.repository.PostsRepository;
 import io.swagger.annotations.ApiOperation;
@@ -15,7 +15,6 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 
 @RestController()
@@ -26,8 +25,8 @@ public class PostsResource {
     PostsController postController;
     UsersRepository usersRepository;
 
-    public PostsResource(PostsRepository post_repository, UsersRepository users_repository){
-        postController = new PostsController(post_repository, users_repository);
+    public PostsResource(PostsRepository post_repository, UsersRepository users_repository, ContentRepository content_repository){
+        postController = new PostsController(post_repository, users_repository, content_repository);
         this.usersRepository = users_repository;
     }
 
@@ -37,7 +36,7 @@ public class PostsResource {
             @ApiResponse(code = 200, message = "Post criado com sucesso"),
             @ApiResponse(code = 500, message = "Ocorreu um erro para processar a requisição")
     })
-    public ResponseEntity<PostDto> createPost(@RequestBody CreatedPostDto newPost) throws NoSuchAlgorithmException {
+    public ResponseEntity<PostDto> createPost(@RequestBody CreatedPostDto newPost) {
 
         try {
             Posts post = postController.createPost(newPost.toPost(usersRepository));
@@ -46,6 +45,7 @@ public class PostsResource {
             }
             return new ResponseEntity(post, HttpStatus.OK);
         } catch (Exception e) {
+            e.printStackTrace();
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
@@ -54,14 +54,18 @@ public class PostsResource {
     @ApiOperation(value = "Todos os Posts", consumes = MediaType.APPLICATION_JSON_VALUE)
     @ApiResponses({
             @ApiResponse(code = 200, message = "Posts retornados com sucesso"),
+            @ApiResponse(code = 204, message = "Não existe nenhum post salvo"),
             @ApiResponse(code = 500, message = "Ocorreu um erro para processar a requisição")
     })
-    public ResponseEntity<String> getAllPosts(){
+    public ResponseEntity<ArrayList<Posts>> getAllPosts(){
 
         try {
             ArrayList<Posts> todos_posts = postController.getAllPosts();
 
-            return new ResponseEntity(todos_posts, HttpStatus.NO_CONTENT);
+            if(todos_posts.isEmpty())
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+
+            return new ResponseEntity<>(todos_posts, HttpStatus.OK);
 
         } catch (IllegalAccessError e) {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
