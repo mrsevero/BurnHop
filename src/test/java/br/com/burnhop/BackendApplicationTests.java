6f87e7819028ef615a7aaf91f7deed7f08a2c6b5
wandsu <wandersonbezerralima@gmail.com>
2020-10-28 19:21:11 -0300

    Iniciado desenvolvimento de testes

--- src/test/java/br/com/burnhop/BackendApplicationTests.java
+++ src/test/java/br/com/burnhop/BackendApplicationTests.java
@@ -1,10 +1,23 @@
 package br.com.burnhop;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+
+
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
+@AutoConfigureMockMvc
 class BackendApplicationTests {
+
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@Autowired
+    private ObjectMapper objectMapper;
 
 	@Test
 	void contextLoads() {
