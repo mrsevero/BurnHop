aca977b3a044346439ad667f6a86709a3632e759
wandsu <wandersonbezerralima@gmail.com>
2020-11-07 19:28:36 -0300

    #56 Adicionado arquivo de testes de requisições dos posts

---
+++ src/test/java/br/com/burnhop/PostsResourceTest.java
@@ -0,0 +1,50 @@
+package br.com.burnhop;
+
+import java.sql.Date;
+import java.sql.Timestamp;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import br.com.burnhop.model.Users;
+import br.com.burnhop.model.Login;
+import br.com.burnhop.model.Dto.CreatedLoginDto;
+import br.com.burnhop.model.Dto.CreatedUserDto;
+import br.com.burnhop.model.Dto.UserDto;
+import br.com.burnhop.api.resource.UsersResource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.http.MediaType;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+
+
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import org.mockito.MockitoAnnotations;
+import org.mockito.Mock;
+
+
+import br.com.burnhop.repository.LoginRepository;
+import br.com.burnhop.repository.UsersRepository;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+class PostsResourceTest{
+    
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+    
+}
