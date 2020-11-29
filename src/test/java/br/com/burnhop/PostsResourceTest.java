133227a271c54d7d28d6104cacd8d8ef13b8251d
wandsu <wandersonbezerralima@gmail.com>
2020-11-29 20:19:07 -0300

    #94 Adicionado testes para pegar todos os posts

--- src/test/java/br/com/burnhop/PostsResourceTest.java
+++ src/test/java/br/com/burnhop/PostsResourceTest.java
@@ -11,15 +11,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import br.com.burnhop.model.dto.CreatedPostDto;
 import br.com.burnhop.model.Login;
 import br.com.burnhop.model.Users;
+import br.com.burnhop.model.Posts;
+import br.com.burnhop.model.Content;
 import br.com.burnhop.repository.LoginRepository;
 import br.com.burnhop.repository.UsersRepository;
+import br.com.burnhop.repository.PostsRepository;
+import br.com.burnhop.repository.ContentRepository;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.http.MediaType;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -36,7 +43,13 @@ class PostsResourceTest {
 	private LoginRepository loginRepository;
 	
 	@Autowired
-    private UsersRepository usersRepository;
+	private UsersRepository usersRepository;
+	
+	@Autowired
+	private PostsRepository postsRepository;
+	
+	@Autowired
+    private ContentRepository contentRepository;
 
 	private void saveUser(String name){
 		Date data_nasc = Date.valueOf("2000-01-01");
@@ -49,8 +62,6 @@ class PostsResourceTest {
 		loginRepository.save(newUser.getLogin());
 		usersRepository.save(newUser);
     }
-
-
 
     private CreatedPostDto makePost(String email){
 	
@@ -74,15 +85,25 @@ class PostsResourceTest {
                 .andExpect(status().isOk());
     }
 
-    /*
+    
 	@Test
 	void testGetAllPostRequest() throws Exception{
-        saveUser("getallposts");
+		saveUser("getallposts");
+		Users users = usersRepository.findByEmail("getallposts@email.com");
+
+		Content content = new Content();
+		content.setText("Getting all posts");
+
+		Posts post = new Posts();
+		post.setContent(content);
+		post.setPostedOn(new Timestamp(System.currentTimeMillis()));
+		post.setUsers(users);
+
+		contentRepository.save(content);
+        postsRepository.save(post);
 
 		mockMvc.perform(get("/posts/get-all"))
 				.andExpect(status().isOk())
 				.andExpect(content().contentType(MediaType.APPLICATION_JSON));
-
-		//Testar limpar o banco
-	}*/
+	}
 }
